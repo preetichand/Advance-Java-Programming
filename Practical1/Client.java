169ede6c97b460c15847eba1352790aae3898899
preeti_chand <preeti07chand@gmail.com>
2021-01-29 20:00:54 +0530

    One way communication

---
+++ Practical1/Client.java
@@ -0,0 +1,23 @@
+package Practical1;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String []args){
+        try{
+            Socket s=new Socket("localhost",8080);
+            DataOutputStream dout=new 
+            DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hello Server,This is Preeti Chand !");
+
+            dout.flush();
+            dout.close();
+            s.close();
+
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
